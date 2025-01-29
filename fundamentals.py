ffe449b24a0e0286087cc61cf0a8daeaff0c35bb
VenkataChalamaReddy <99597382+VenkataChalamaReddy@users.noreply.github.com>
2025-01-29 18:05:52 +0530

    Add files via upload
    Added a file by uploading

---
+++ fundamentals.py
@@ -0,0 +1,31 @@
+#print  ---- to display information on the screen
+#Syntax
+#print(Expression)
+#print(10+10)
+
+
+#Comments --> Comments are used to explain the code
+#single line comments starts with # where as multiple line comments start and end with ''' or """"
+#Below code is used to perform Addituion operation
+num_1=10
+num_2=20
+result=num_1+num_2     #here addition is done and this is inline comment
+print(result)       
+
+'''
+I have taken two variables
+and i assigned values to those variables
+and i perform adition operation by using + operator
+later, i display the result by using print function'''
+
+
+
+
+#Variables
+#Syntax
+#variable = Value
+
+roll_number=123
+print(roll_number)
+
+
